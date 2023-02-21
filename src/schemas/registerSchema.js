a87a8857f2c7ac3c2006bcd3a0e40aad40ce59ee
MatheusFerreiraL <matheusfleandro@gmail.com>
2023-02-21 15:02:24 -0300

    :sparkles: feat: esquema de login

--- src/schemas/registerSchema.js
+++ src/schemas/registerSchema.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
-export const registerSchema = Yup.object().shape({
+const registerSchema = Yup.object().shape({
   storeName: Yup.string().required('* Store name is required'),
   email: Yup.string().email('Invalid email').required('* Email is required'),
   password: Yup.string().required('* Password is required'),
@@ -10,7 +10,4 @@ export const registerSchema = Yup.object().shape({
   ),
 });
 
-export const loginSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('*Email is required'),
-  password: Yup.string().required('*Password is required'),
-});
+export default registerSchema;

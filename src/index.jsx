8650b186085186117eb6557feb2d99f4f0da91d5
MatheusFerreiraL <matheusfleandro@gmail.com>
2023-02-23 16:09:48 -0300

    :sparkles: feat: criacao de contexto para props

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import MainRoutes from './routes';
+import { UserProvider } from './contexts/userProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <MainRoutes />
+      <UserProvider>
+        <MainRoutes />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );

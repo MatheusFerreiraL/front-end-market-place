5e813eb50ebad97a9a820a56bd0df070dc3198e4
MatheusFerreiraL <matheusfleandro@gmail.com>
2023-02-25 20:44:10 -0300

    :sparkles: feat: pagina home

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,35 @@
+import CardMedia from '@mui/material/CardMedia';
+import { Box } from '@mui/system';
+import { styled } from '@mui/material/styles';
+import { css } from '@mui/styled-engine';
+import Card from '@mui/material/Card';
+
+export const ProductCardMedia = styled(CardMedia)`
+  max-width: 342px;
+  max-height: 342px;
+  margin: 20px 20px 0 20px;
+`;
+
+export const ContainerProductCardMedia = styled(Box)`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    gap: 48px;
+    flex-wrap: wrap;
+    margin-right: 117px;
+    margin-bottom: 160px;
+    margin-left: 117px;
+    color: ${theme.palette.primary.main};
+  `}
+`;
+
+export const CustomCard = styled(Card)`
+  width: 382px;
+  transition: 0.4s ease-in;
+  :hover {
+    transform-origin: center;
+    transform: scale(110%);
+    z-index: 0;
+    transition-duration: 0.4s;
+  }
+`;

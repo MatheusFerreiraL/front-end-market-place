55d8ff1bff7a41b96ae5768ad7aae9bc285aa0db
MatheusFerreiraL <matheusfleandro@gmail.com>
2023-02-25 20:50:11 -0300

    :sparkles: feat: rotas protegidas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,36 @@
 import { ThemeProvider } from '@mui/material/styles';
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import GlobalStyles from './styles/GlobalStyles';
 import standardTheme from './theme/standardTheme';
+import { getItem } from './utils/storage';
+import Header from './components/Header';
+
+function ProtectedRoutes({ redirectTo }) {
+  const isAuthenticated = getItem('token');
+
+  console.log(isAuthenticated);
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />;
+}
 
 export default function MainRoutes() {
   return (
     <ThemeProvider theme={standardTheme}>
       <GlobalStyles />
       <Routes>
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Header />}>
+          <Route path='' element={<Home />} />
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
+        <Route element={<ProtectedRoutes redirectTo='/' />}>
+          <Route path='/' element={<Header />}>
+            <Route path='' element={<Home />} />
+          </Route>
+        </Route>
       </Routes>
     </ThemeProvider>
   );

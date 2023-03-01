3d986ec7ec92fd41295b411ebf593628e5ac2b3f
MatheusFerreiraL <matheusfleandro@gmail.com>
2023-02-28 23:12:04 -0300

    :sparkles: feat: ajuste de consumo de api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,27 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import { clearAll } from '../utils/storage';
 
-export default axios.create({
+const axiosPrivate = axios.create({
   baseURL: 'http://localhost:8000',
   timeout: 1000,
   headers: { 'Content-Type': 'application/json' },
 });
+
+axiosPrivate.interceptors.response.use(
+  response => response,
+  async error => {
+    if (error?.response?.status === 401) {
+      toast.error(error.response.data.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      setTimeout(() => {
+        window.location.replace('http://localhost:3000/login');
+        clearAll();
+      }, 2500);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosPrivate;
